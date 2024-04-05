e30c3677d1eed505b6626f880fada770b142fc88
Wellington Batista <wellingtonbs109@gmail.com>
2024-04-04 23:11:53 -0300

    feat: update login and signup route

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,16 +2,19 @@ import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { AuthService } from './auth.service';
+import { HttpService } from './httpSevice.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class LoginService implements OnInit {
+  port = this.httpService.getPort();
 
   constructor(
     private http: HttpClient,
-    private authService: AuthService
+    private authService: AuthService,
+    private httpService: HttpService
   ) { }
 
   ngOnInit(): void {
@@ -19,7 +22,7 @@ export class LoginService implements OnInit {
   }
 
   login(userData: any): Observable<any> {
-    return this.http.post(`http://localhost:5000/login`, userData)
+    return this.http.post(`${this.port}/login`, userData )
   }
 
 
--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpService } from './httpSevice.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
+  port = this.httpService.getPort();
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private httpService: HttpService
   ) { }
 
   signup(userData: any) {
-    return this.http.post('http://localhost:5000/signup', userData)
+    return this.http.post(`${this.port}/signup`, userData )
   }
 }
